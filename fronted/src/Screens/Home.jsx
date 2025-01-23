91ead44db00f9e0e92e255a2059a0cfe2a4aa4d9
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-23 21:10:50 +0530

    all project show into UI

--- fronted/src/Screens/Home.jsx
+++ fronted/src/Screens/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../context/user.context'
 import axios from "../config/axios"
 
@@ -7,6 +7,7 @@ const Home = () => {
     const { user } = useContext(UserContext)
     const [ isModalOpen, setIsModalOpen ] = useState(false)
     const [ projectName, setProjectName ] = useState(null)
+    const [project, setproject] = useState([])
 
     function createProject(e) {
         e.preventDefault()
@@ -24,15 +25,39 @@ const Home = () => {
             })
     }
 
+    useEffect(() => {
+      axios.get('/projects/all').then((res) => {    
+                console.log(res);
+                setproject(res.data.projects);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }, [])
+    
+
     return (
         <main className='p-4'>
             <div className="projects">
                 <button
                     onClick={() => setIsModalOpen(true)}
-                    className="project p-4 border border-slate-300 rounded-md">
+                    className="project p-4 border border-slate-300 rounded-md font-semibold">
                     New Project
                     <i className="ri-link ml-2"></i>
                 </button>
+                <div className='gap-4 grid grid-cols-3 mt-4 '>
+           {project.map((pro) => {
+           return (
+            <div key={pro._id} className="hover:bg-zinc-200 hover:shadow-mdproject p-4 border cursor-pointer border-slate-300 rounded-md">
+                {pro.name}
+                <div className="mt-2">
+                    <h1><i class="ri-user-add-line"></i> Collaborators:  {pro.users.length} </h1>
+                   
+                </div>
+            </div>
+              );
+               })}
+           </div>
+
             </div>
 
             {isModalOpen && (
